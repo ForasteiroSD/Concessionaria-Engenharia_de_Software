abef1eb6906902160482f78fdf6c800e98e23f32
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2023-05-19 18:14:06 -0300

    Página inicial da implementação adicionada

---
+++ implementacao/codigos/form.php
@@ -0,0 +1,9 @@
+<?php
+
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+
+    echo '<script>alert("OI")</script>';
+    echo '<script>window.location.href = "login.php"</script>';
+
+    ?>
