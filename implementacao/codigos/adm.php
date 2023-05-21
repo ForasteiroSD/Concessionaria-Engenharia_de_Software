4c2d87df78d6be881d305d31553485b88836231c
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2023-05-20 23:23:12 -0300

    Interfaces de consulta e inserção adicionadas
    Interfaces de consulta e inserção da página de administrador e da página de gerente, na área de funcionários, adicionadas.

--- implementacao/codigos/adm.php
+++ implementacao/codigos/adm.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset='utf-8'>
-    <title>Fita | Gerente</title>
+    <title>Fita | Administrador</title>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
 </head>
 <body>
@@ -16,7 +16,7 @@
                 $agora = new DateTime('now', $timezone);
                 echo '<div id="date">
                         <h3>' . $agora->format("d/m") . ' | ' . $agora->format("H:i") . '</h3>
-                        <img src="../imgs/logout_button.png" alt="Logo">
+                        <a href="./login.html"><img src="../imgs/logout_button.png" alt="Logo"></a>
                       </div>'
             ?>
 
@@ -48,8 +48,8 @@
                     <input type="text" name="user" />
                 </div>
                 <div>
-                <label>Cargo:</label>
-                    <select name="cargo" id="cargo">
+                <label>Tipo:</label>
+                    <select name="tipo" id="cargo">
                         <option value="todos">Todos</option>
                         <option value="vendedor">Vendedor</option>
                         <option value="gerente">Gerente</option>
@@ -66,20 +66,79 @@
             </form>
             <hr>
 
+            <div class="atrib_ret">
+                <h3>Nome</h3>
+                <h3>CPF</h3>
+                <h3>User</h3>
+                <h3>Tipo</h3>
+            </div>
+
+            <div>
+                <div class='borders'></div>
+                <div class='borders'></div>
+                <div class='borders'></div>
+            </div>
+
             <div class="resultados">
 
             </div>
         </div>
 
-        <button id="add_button">
+        <button id="add_button" onclick="insert()">
             Adicionar novo Usuário
         </button>
-        
+
+        <div id="back"></div>
+        <div id="screen">
+            <div id="add_new">
+                <form action="" class="form_new">
+                    <h2>Inserir novo Usuário</h2>
+                    <img src="../imgs/close_button.png" alt="Fechar Inserir Usuário" onclick="closeForm()">
+                    <div>
+                        <label>CPF:</label>
+                        <input type="number" name="cpf" requiered/>
+                    </div>
+                    <div>
+                        <label>User:</label>
+                        <input type="text" name="user" requiered/>
+                    </div>
+                    <div>
+                        <label>Senha:</label>
+                        <input type="text" name="senha" requiered/>
+                    </div>
+                    <div>
+                    <label>Tipo:</label>
+                        <select name="tipo" id="cargo">
+                            <option value="vendedor">Vendedor</option>
+                            <option value="gerente">Gerente</option>
+                            <option value="mecanico">Mecânico</option>
+                            <option value="secretario">Secretário</option>
+                            <option value="adm">Administrador</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button type="submit">
+                            Inserir
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
      
 
 </body>
 <script>
-    
+    function closeForm(){
+        document.getElementById("back").style.display = "none"
+        document.getElementById("screen").style.visibility = "hidden"
+        document.getElementById("add_new").style.opacity = "0"
+    }
+
+    function insert(){
+        document.getElementById("back").style.display = "block"
+        document.getElementById("screen").style.visibility = "visible"
+        document.getElementById("add_new").style.opacity = "1"
+    }
 </script>
 </html>
 
--- implementacao/codigos/gerente.php
+++ implementacao/codigos/gerente.php
@@ -16,7 +16,7 @@
                 $agora = new DateTime('now', $timezone);
                 echo '<div id="date">
                         <h3>' . $agora->format("d/m") . ' | ' . $agora->format("H:i") . '</h3>
-                        <img src="../imgs/logout_button.png" alt="Logo">
+                        <a href="./login.html"><img src="../imgs/logout_button.png" alt="Logo"></a>
                       </div>'
             ?>
 
@@ -67,20 +67,97 @@
             </form>
             <hr>
 
+            <div class="atrib_ret">
+                <h3>Nome</h3>
+                <h3>CPF</h3>
+                <h3>Cargo</h3>
+            </div>
+
+            <div>
+                <div class='borders'></div>
+                <div class='borders'></div>
+                <div class='borders'></div>
+            </div>
+
             <div class="resultados">
 
             </div>
         </div>
 
-        <button id="add_button">
+        <button id="add_button" onclick="insert()">
             Adicionar novo Funcionário
         </button>
+
+        <div id="back"></div>
+        <div id="screen">
+            <div id="add_new">
+                <form action="" class="form_new">
+                    <h2>Inserir novo Funcionário</h2>
+                    <img src="../imgs/close_button.png" alt="Fechar Inserir Funcionário" onclick="closeForm()">
+                    <div>
+                        <label>Nome:</label>
+                        <input type="text" name="name" requiered/>
+                    </div>
+                    <div>
+                        <label>CPF:</label>
+                        <input type="number" name="cpf" requiered/>
+                    </div>
+                    <div>
+                        <label>Data de nascimento:</label>
+                        <input type="date" name="data" requiered/>
+                    </div>
+                    <div>
+                        <label>Telefone:</label>
+                        <input type="text" name="telefone" placeholder="(XX) XXXXXXXXX" requiered/>
+                    </div>
+                    <div>
+                        <label>Salário:</label>
+                        <input type="number" name="salario" requiered/>
+                    </div>
+                    <div>
+                        <label>Data de Contratação:</label>
+                        <input type="date" name="salario" 
+                        value="<?php
+                                    $timezone = new DateTimeZone('America/Sao_Paulo');
+                                    $agora = new DateTime('now', $timezone); 
+                                    echo $agora->format('Y-m-d'); 
+                                ?>"
+                        requiered/>
+                    </div>
+                    <div>
+                    <label>Cargo:</label>
+                        <select name="tipo" id="cargo">
+                            <option value="vendedor">Vendedor</option>
+                            <option value="gerente">Gerente</option>
+                            <option value="mecanico">Mecânico</option>
+                            <option value="secretario">Secretário</option>
+                            <option value="adm">Administrador</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button type="submit">
+                            Inserir
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
         
      
 
 </body>
 <script>
-    
+    function closeForm(){
+        document.getElementById("back").style.display = "none"
+        document.getElementById("add_new").style.visibility = "hidden"
+        document.getElementById("add_new").style.opacity = "0"
+    }
+
+    function insert(){
+        document.getElementById("back").style.display = "block"
+        document.getElementById("add_new").style.visibility = "visible"
+        document.getElementById("add_new").style.opacity = "1"
+    }
 </script>
 </html>
 
